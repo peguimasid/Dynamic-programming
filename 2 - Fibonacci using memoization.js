cb96905e36b50e4102bab006f7ca471637822a26
Guilhermo Masid <guilhermomasid@gmail.com>
2021-07-03 20:02:09 -0300

    fibonacci using memo

---
+++ 2 - Fibonacci using memoization.js
@@ -0,0 +1,9 @@
+function fib(n, memo = {}) {
+  if (memo[n]) return memo[n];
+  if (n <= 2) return 1;
+  let res = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
+
+console.log(fib(100));
